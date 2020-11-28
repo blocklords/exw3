5953310a2d8ea05305f6e2d8f235d16402c7088f
Alex <alex+git@fremantle.io>
2020-11-27 16:35:19 -0800

    WIP: Start accepting client_type & url override to block_number

--- test/exw3/rpc_test.exs
+++ test/exw3/rpc_test.exs
@@ -1,8 +1,15 @@
 defmodule ExW3.RpcTest do
   use ExUnit.Case
-  doctest ExW3.Rpc
+
+  @http_rpc_url "http://foo/bar"
+  @ipc_rpc_url "/foo/bar"
 
   test ".block_number/0 " do
-    assert ExW3.block_number() > 0
+    assert ExW3.block_number() |> is_integer
+  end
+
+  test ".block_number/1 can override the client" do
+    assert ExW3.block_number(client_type: :http, url: @http_rpc_url) |> is_integer
+    assert ExW3.block_number(client_type: :ipc, url: @ipc_rpc_url) |> is_integer
   end
 end
